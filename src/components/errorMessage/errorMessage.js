3d790fbd6b7b551760330423ecd21a272006093f
Хоменко Олена Володимирівна <Khomenko@khnpp.ua>
2023-03-30 10:46:32 +0300

    add 4-th commit

---
+++ src/components/errorMessage/errorMessage.js
@@ -0,0 +1,11 @@
+import img from './error.gif';
+import './errorMesage.scss';
+
+const ErrorMessage = () => {
+    return (
+        <img src={img} alt="error" />
+    )
+
+}
+
+export default ErrorMessage;
--- src/components/services/MarvelService.js
+++ src/components/services/MarvelService.js
@@ -1,8 +1,9 @@
-class MarvelService{
+class MarvelService {
 	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
 	_apiKey = 'apikey=502a8f9406329dde74089757c4624e16';
 	getResource = async (url) => {
 		const res = await fetch(url);
+		//console.log(res.status);//отримання статусу http
 		if (!res.ok) {
 			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
 		}
@@ -20,12 +21,21 @@ class MarvelService{
 		return this._transformCharacter(res.data.results[0]);
 	}
 
+	getAllId = async () => {
+		const res = await this.getResource(`${this._apiBase}characters?orderBy=name&${this._apiKey}`);
+
+		return res.data.results.map(this._transformCharacter);
+	}
+
+
 	_transformCharacter = (char) => {
-		return   {name: char.name,
-					description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
-					thumbnail: char.thumbnail.path+'.' + char.thumbnail.extension,
-					homePage: char.urls[0].url,
-					wiki: char.urls[1].url
+		return {
+			name: char.name,
+			description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+			homePage: char.urls[0].url,
+			wiki: char.urls[1].url,
+			id: char.id
 		}
 	}
 }
